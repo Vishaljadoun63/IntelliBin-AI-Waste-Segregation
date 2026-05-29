509beb42e5d5b4acced552153aa1e28e1cee4f53
Vishal Jadoun <jadounvishal16@gmail.com>
2026-05-29 17:14:48 +0530

    Retrained model and updated training script

--- train_model.py
+++ train_model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
